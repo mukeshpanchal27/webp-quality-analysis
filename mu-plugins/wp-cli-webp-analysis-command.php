3f7cb7d9fa5dcdcefd2a30881fcf8e033cf7ae51
jjgrainger <github@jjgrainger.co.uk>
2022-08-24 12:12:39 +0100

    add options to command

--- mu-plugins/wp-cli-webp-analysis-command.php
+++ mu-plugins/wp-cli-webp-analysis-command.php
@@ -16,48 +16,88 @@ class Webp_Analysis_Command {
     /**
      * Runs the WebP Quality Analysis
      *
+     * ## OPTIONS
+     *
+     * [--limit=<limit>]
+     * : Set the total number of attachments to analyse
+     * ---
+     * default: 500
+     * ---
+     *
+     * [--preview]
+     * : Preview results as an ASCII table
+     *
+     * ## EXAMPLES
+     *
+     *     wp webp-analysis run --limit=10
+     *
      * @when after_wp_load
      */
     function run( $args, $assoc_args ) {
+        // Preview results in an ASCII table.
+        $preview = isset( $assoc_args['preview'] ) ? true : false;
 
+        // Set the correct limit. When previewing results limit total attachments to 5
+        // otherwise use the limit passed.
+        $limit = $preview ? min( (int) $assoc_args['limit'], 5 ) : (int) $assoc_args['limit'];
+
+        // Create WP_Query to retrieve attachment IDs.
         $query_args = [
             'post_type'      => 'attachment',
             'post_status'    => 'any',
             'fields'         => 'ids',
-            'posts_per_page' => 500,
+            'posts_per_page' => $limit,
         ];
 
         $query = new WP_Query( $query_args );
 
-        // Create a rows array
+        if ( empty( $query->posts ) ) {
+            WP_CLI::error( __( 'No attachments found.') );
+        }
+
+        WP_CLI::line( sprintf( __( '%d attachments found.', 'webp-quality-analysis' ), count( $query->posts ) ) );
+
+        // Create a items array.
         $items = array();
 
         foreach ( $query->posts as $post_id ) {
             $metadata = wp_get_attachment_metadata( $post_id );
 
+            if ( ! isset( $metadata['sources']['image/webp'] ) ) {
+                WP_CLI::error( sprintf( __( 'Skipping attachment %d, no WebP data found.', 'webp-quality-analysis' ), $post_id ), false );
+                continue;
+            }
 
             foreach ( $metadata['sizes'] as $size => $data ) {
                 $items[] = array(
-                    'ID'             => $post_id,
-                    'filename'       => $data['file'],
-                    'size'           => $size,
-                    'width'          => $data['width'],
-                    'height'         => $data['height'],
-                    'jpeg_filesize'  => $data['sources']['image/jpeg']['filesize'],
-                    'webp_filesize'  => $data['sources']['image/webp']['filesize'],
-                    'is_webp_larger' => ( $data['sources']['image/jpeg']['filesize'] < $data['sources']['image/webp']['filesize'] ),
+                    'ID'            => $post_id,
+                    'filename'      => $data['file'],
+                    'size'          => $size,
+                    'width'         => $data['width'],
+                    'height'        => $data['height'],
+                    'jpeg_filesize' => $data['sources']['image/jpeg']['filesize'],
+                    'webp_filesize' => $data['sources']['image/webp']['filesize'],
+                    'larger_webp'   => ( $data['sources']['image/jpeg']['filesize'] < $data['sources']['image/webp']['filesize'] ) ? 1 : 0,
                 );
             }
         }
 
-        // To CSV
-        $filepath = plugin_dir_path( __FILE__ ) . "../exports/webp-quality-analysis-" . time() . ".csv";
-        $file = fopen( $filepath, 'w' );
+        if ( empty( $items ) ) {
+            WP_CLI::error( __( 'No data collected.', 'webp-quality-analysis' ) );
+        }
 
+        // Display results in an ASCII table.
+        if ( $preview ) {
+            format_items( 'table', $items, array_keys( $items[0] ) );
+            exit;
+        }
+
+        // Write results to CSV file with timestamp.
+        $filename = plugin_dir_path( __FILE__ ) . "../exports/" . time() . "-webp-quality-analysis.csv";
+        $file     = fopen( $filename, 'w' );
         write_csv( $file, $items, array_keys( $items[0] ) );
 
-        // Organise metadata into rows.
-        // format_items( 'table', $items, array_keys( $items[0] ) );
+        WP_CLI::success( sprintf( __( 'WebP analysis export created: %s', 'webp-quality-analysis' ), wp_basename( $filename ) ) );
     }
 }
 
